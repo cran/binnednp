cc19d5edcf38b9a9d0ba3a27545467b54db7f1d2
Daniel Barreiro Ures <dbu5293@gmail.com>
2019-06-07 16:50:03 +0000

    version 0.4.0

--- R/anv.binned.R
+++ R/anv.binned.R
@@ -1,29 +1,29 @@
-#' ANOVA in kernel distribution estimation with binned data.
-#'
+#' ANOVA in kernel distribution estimation with binned data using bootstrap.
+#' 
 #' @param n Vector of positive integers. Sizes of the complete samples corresponding to each treatment.
 #' @param y Vector. Observed values. They define the extremes of the sequence of intervals in which data is binned.
 #' @param trt.w Matrix. Proportion of observations within each interval. Each column corresponds to a different treatment.
 #' @param abs.values Logical. Indicates if the values of trt.w are given in absolute (TRUE) or relative (FALSE) format.
-#' @param alpha Real number between 0 and 1. Significance level of the test.
 #' @param B Positive integer. Number of bootstrap replicates used to compute the confidence bands.
 #' 
 #' @details 
-#' Constructs bootstrap confidence bands for each treatment and checks whether they overlap or not.
+#' ANOVA for interval-grouped data.
 #' 
-#' @return TRUE if the null hypothesis is accepted and FALSE otherwise.
+#' @return p-value of the test.
 #' 
 #' @references 
 #' \insertRef{TesisMiguel2015}{binnednp}
-#'
+#' 
 #' @useDynLib binnednp
 #' @importFrom Rcpp sourceCpp
-#'
+#' 
 #' @export
-anv.binned <- function(n,y,trt.w,abs.values=FALSE,alpha=0.05,B=500)
+anv.binned <- function(n,y,trt.w,abs.values=FALSE,B=500)
 {
   
   ntrt <- ncol(trt.w)
-  alpha2 <- alpha/ntrt
+  lwp <- nrow(trt.w)
+  npooled <- sum(n)
   if(abs.values)
   {
     for(i in 1:ntrt)
@@ -32,31 +32,61 @@ anv.binned <- function(n,y,trt.w,abs.values=FALSE,alpha=0.05,B=500)
     }
   }
   
-  ncores <- parallel::detectCores()
-  cl <- parallel::makeCluster(ncores)
-  doParallel::registerDoParallel(cl)
-  parout <- foreach::foreach(i=1:ntrt,.combine=rbind,.packages=c("Rcpp","binnednp")) %dopar%{
-    band.boot <- bw.dist.binned.boot(n[i],y,trt.w[,i],confband=TRUE,alpha=alpha2,plot=FALSE,print=FALSE,parallel=FALSE,B=B)$confband
-    return(band.boot)
-  }
-  
-  parallel::stopCluster(cl)
-  H0.boot <- TRUE
-  maxmin.i <- numeric(ntrt)
-  minmax.i <- numeric(ntrt)
-  kk <- length(y)
-  for(i in 1:kk)
+  wpooled <- numeric(lwp)
+  for(i in 1:lwp)
   {
-    for(j in 1:ntrt)
+    wpooled[i] <- sum(trt.w[i,]*n)
+  }
+  wpooled <- wpooled/npooled
+  
+  t <- y[-length(y)]+diff(y)/2
+  lim2 <- t[length(t)]
+  lim1 <- t[1]
+  obj <- bw.dist.binned(npooled,y,wpooled,print=FALSE,plot=FALSE)
+  hpooled <- obj$h
+  Fh <- obj$Fh
+  D <- 0
+  fh <- Vectorize(function(x)1/hpooled*sum(wpooled*stats::dnorm((x-t)/hpooled)))
+  for(i in 1:ntrt)
+  {
+    Fhi <- bw.dist.binned(n[i],y,trt.w[,i],plot=FALSE,print=FALSE)$Fh
+    D <- D+stats::integrate(function(x)(Fhi(x)-Fh(x))^2*fh(x),lim1+hpooled*stats::qnorm(0.99),lim2+hpooled*stats::qnorm(0.01))$value*n[i]
+  }
+  D <- D/ntrt
+  
+  
+  Db <- numeric(B)
+  for(b in 1:B)
+  {
+    cat("\r",b)
+    wbi <- matrix(NA,nrow=lwp,ncol=ntrt)
+    wbpooled <- numeric(lwp)
+    for(i in 1:ntrt)
     {
-      maxmin.i[j] <- max(parout[i+kk*(j-1),2])
-      minmax.i[j] <- min(parout[i+kk*(j-1),1])
+      xbi <- sort(sample(t,size=n[i],prob=wpooled,replace=TRUE)+hpooled*stats::rnorm(n[i]))
+      wbi[,i] <- calcw_cpp(xbi,y)
     }
-    H0.boot <- ifelse(any(maxmin.i>minmax.i),FALSE,TRUE)
-    if(H0.boot == FALSE) break
+    for(i in 1:lwp)
+    {
+      wbpooled[i] <- sum(wbi[i,]*n)
+    }
+    wbpooled <- wbpooled/npooled
+    objb <- bw.dist.binned(npooled,y,wbpooled,print=FALSE,plot=FALSE)
+    hb <- objb$h
+    Fhb <- objb$Fh
+    fhb <- Vectorize(function(x)1/hb*sum(wbpooled*stats::dnorm((x-t)/hb)))
+    for(i in 1:ntrt)
+    {
+      Fhbi <- bw.dist.binned(n[i],y,wbi[,i],print=FALSE,plot=FALSE)$Fh
+      Db[b] <- Db[b]+stats::integrate(function(x)(Fhbi(x)-Fhb(x))^2*fhb(x),lim1+hb*stats::qnorm(0.99),lim2+hb*stats::qnorm(0.01))$value*n[i]
+    }
+    
   }
   
-  return(H0.boot)
+  Db <- Db/ntrt
   
+  pvalue <- mean(Db>D)
+  
+  return(pvalue)
   
 }
